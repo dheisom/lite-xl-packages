020d359b41e6e56be22900079f457ab253f7437c
Dheisom Gomes <dheisomgomes0@gmail.com>
2022-03-02 17:16:55 -0300

    gitdiff added

---
+++ packages/gitdiff.lua
@@ -0,0 +1,19 @@
+local net = require 'plugins.lite-xl-pm.net'
+local logger = require 'plugins.lite-xl-pm.logger'
+
+local gitdiff = logger:new("[gitdiff]")
+local url = "https://github.com/vincens2005/lite-xl-gitdiff-highlight.git"
+
+gitdiff:log("Clonning lite-xl-gitdiff-highlight...")
+net.clone(
+  url,
+  USERDIR.."/plugins/gitdiff_highlight",
+  function(ok)
+    if ok then
+      gitdiff:log("Installed with success! Restart to work.")
+    else
+      gitdiff:error("Failed to clone lite-xl-gitdiff-highlight!")
+    end
+  end
+)
+
